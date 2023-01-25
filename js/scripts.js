2aa4b1d9e3fdc9471b47efc3d167fe537fc7f876
9dcw <davecwright@gmail.com>
2023-01-25 18:59:28 -0500

    contact form!

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,9 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -23,7 +22,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
@@ -57,3 +56,93 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+
+function submitEmail() {
+  sender = document.getElementById('email').value
+  console.log('submitting email '+ sender)
+  let form = new FormData();
+  form.append('email',sender);
+  u = 'https://meaningful-texting-aqtiusbaaq-uc.a.run.app/email_signup'
+  u2 = 'https://harvesting.ninja/mdw_contact_form'
+  fetch(u2, {
+      method: 'POST',
+      body: form
+  })
+      .then(function(response) {
+
+          console.log('received clip')
+          document.getElementById('submitSuccessMessage').style.display='inline'
+          return response.text()
+      } )
+      .catch(function(error) {
+        document.getElementById('submitErrorMessage').style.display='inline'
+
+        console.log('error ' + String(error))
+        }
+      )
+}
+
+
+function checkemail() {
+
+  var emailText = document.getElementById('email').value
+  var valid = ValidateEmail(emailText)
+  var submitbtn = document.getElementById('submitButton')
+  var email_label = document.getElementById('email_label')
+  console.log(emailText)
+  //var errortext = document.getElementById('invalidEmail')
+  //var successtext = document.getElementById('validEmail')
+
+  //var formNotice = document.getElementById('emailFormNotice')
+  //console.log(emailText)
+  if (!emailText) {
+      //formNotice.style.display='inline'
+      //errortext.style.display='none'
+      //successtext.style.display='none'
+      email_label.innerHTML='Please Enter Valid Email Address'
+      submitbtn.innerHTML = 'Please Enter Valid Email to Submit'
+      submitbtn.setAttribute('class','btn btn-primary')
+      submitbtn.disabled = true
+      return
+  }
+  else {
+    //formNotice.style.display='none'
+  }
+  if (valid=== true) {
+    //errortext.style.display='none'
+    //successtext.style.display='inline'
+    email_label.innerHTML='Valid!'
+    submitbtn.setAttribute('class','btn btn-primary')
+    submitbtn.disabled = false
+    submitbtn.innerHTML = 'Submit'
+
+
+  }
+  else {
+
+    //errortext.style.display='inline'
+    //successtext.style.display='none'
+    email_label.innerHTML='Invalid Email'
+    submitbtn.innerHTML = 'Please Enter Valid Email to Submit'
+    submitbtn.setAttribute('class','btn btn-primary')
+    submitbtn.disabled = true
+
+  }
+
+}
+
+function ValidateEmail(input) {
+  console.log(input)
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (input.match(validRegex)) {
+    return true;
+  } else {
+    //alert("Invalid email address!");
+
+    return false;
+  }
+
+}
